c559c1e082379262da05ba27a41b8feea1e2516d
Hadi <hadi.effatian@gmail.com>
2018-04-04 10:50:16 +0200

    Created project

---
+++ src/main/java/at/refugeescode/mp9_my_personal_todo_list_NoSQL_MongoDB/Mp9MyPersonalTodoListNoSqlMongoDbApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp9_my_personal_todo_list_NoSQL_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp9MyPersonalTodoListNoSqlMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp9MyPersonalTodoListNoSqlMongoDbApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp9_my_personal_todo_list_NoSQL_MongoDB/Mp9MyPersonalTodoListNoSqlMongoDbApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp9_my_personal_todo_list_NoSQL_MongoDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp9MyPersonalTodoListNoSqlMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
